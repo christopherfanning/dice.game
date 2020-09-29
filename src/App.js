b7584e7200700c98b15c7087fab27eb78dc17f8e
chris <chris@christopherfanning.com>
2020-09-29 18:47:21 -0500

    add material ui default css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import './App.css';
-import Button from '@material-ui/core/Button';
-import Player from './components/Player'
-import Game from './components/Game'
+import React from "react";
+import "./App.css";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Button from "@material-ui/core/Button";
+import Player from "./components/Player";
+import Game from "./components/Game";
 
 function App() {
   return (
-    <div className="App">
-    <h1> Welcome to the Thunder Dome dice game</h1>
-    <Player></Player>
-    <Game></Game>
-    <Player></Player>
-    </div>
+    <CssBaseline>
+      <div className="App">
+        <h1> Welcome to the Thunder Dome dice game</h1>
+        <Player></Player>
+        <Game></Game>
+        <Player></Player>
+      </div>
+    </CssBaseline>
   );
 }
 
